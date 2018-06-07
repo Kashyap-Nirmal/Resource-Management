94b56c008d3e9352e4228ba5e4e1820c12abed08
Kashyap-Nirmal <16it059@charusat.edu.in>
2018-06-07 09:05:31 +0530

    Add files via upload

---
+++ Resource_Book.aspx
@@ -0,0 +1,113 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Resource_Book.aspx.cs" Inherits="Resource_Book" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtdate" runat="server" TextMode="Date"></asp:TextBox>
+        <asp:TextBox ID="txtfromtime" runat="server" TextMode="Time"></asp:TextBox>
+        <asp:TextBox ID="txttotime" runat="server" TextMode="Time"></asp:TextBox>
+    
+        <asp:TextBox ID="txtpurpose" runat="server" TextMode="MultiLine"></asp:TextBox>
+    
+    </div>
+        <div>
+
+            <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource2" DataTextField="name" DataValueField="Id" AutoPostBack="True">
+            </asp:DropDownList>
+            <br />
+            <asp:DropDownList ID="DropDownList2" runat="server" AppendDataBoundItems="True" DataSourceID="SqlDataSource3" DataTextField="Room" DataValueField="Id">
+                <asp:ListItem>ALL</asp:ListItem>
+            </asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDataSource3" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT *,(ISNULL(Room_no, '') + ' ' + ISNULL(Name, '')) AS Room FROM [Rooms] WHERE ([Type] = @Type)
+">
+                <SelectParameters>
+                    <asp:ControlParameter ControlID="DropDownList1" DefaultValue="0" Name="Type" PropertyName="SelectedValue" Type="Int32" />
+                </SelectParameters>
+            </asp:SqlDataSource>
+            <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Room_Type]"></asp:SqlDataSource>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Resource_Details]"></asp:SqlDataSource>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Check Availability" />
+
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4" OnRowCommand="GridView1_RowCommand" OnDataBound="GridView1_DataBound">
+            <Columns>
+                <asp:TemplateField HeaderText="Book">
+                    <ItemTemplate>
+                        <asp:Button ID="btnbook" runat="server" CommandArgument='<%# Eval("Id") %>' Text="Book" OnCommand="btnbook_Command" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Availability">
+                    <ItemTemplate>
+                        <asp:Label ID="lblavailability" runat="server" Text='<%# Eval("Availability") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Room No.">
+                    <ItemTemplate>
+                        <asp:Label ID="lblroomno" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Building">
+                    <ItemTemplate>
+                        <asp:Label ID="lblbuilding" runat="server" Text='<%# Eval("Name1") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Strength">
+                    <ItemTemplate>
+                        <asp:Label ID="lblstrength" runat="server" Text='<%# Eval("strength") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Contact Person">
+                    <ItemTemplate>
+                        <asp:Label ID="lblcontact" runat="server" Text='<%# Eval("Name2") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Department">
+                    <ItemTemplate>
+                        <asp:Label ID="lbldept" runat="server" Text='<%# Eval("Department") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Extension">
+                    <ItemTemplate>
+                        <asp:Label ID="lblextension" runat="server" Text='<%# Eval("Extension") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Date">
+                    <ItemTemplate>
+                        <asp:Label ID="lbldate" runat="server" Text='<%# Eval("Date") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="From">
+                    <ItemTemplate>
+                        <asp:Label ID="lblfrom" runat="server" Text='<%# Eval("From") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="To">
+                    <ItemTemplate>
+                        <asp:Label ID="lblto" runat="server" Text='<%# Eval("To") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Purpose">
+                    <ItemTemplate>
+                        <asp:Label ID="lblpurpose" runat="server" Text='<%# Eval("Purpose") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Resource_Book.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Resource_Book : System.Web.UI.Page
+{
+    SqlConnection cn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+         DropDownList2.Items.Clear();
+         DropDownList2.Items.Insert(0, "ALL");
+        }
+            
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (DropDownList2.SelectedIndex == 0)
+        {
+            /*SqlConnection cn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+            //SqlCommand cmd = new SqlCommand("insert into Resource_Allocation values ('" + txtdate.Text + "','" + txtfromtime.Text + "','" + txttotime.Text + "'," + int.Parse(DropDownList1.SelectedValue) + ")",cn);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();*/
+            GridView1.DataSource = null;
+            GridView1.DataBind();
+
+            SqlDataAdapter da_alloc = new SqlDataAdapter("select resource_id,allocation_s_time,allocation_e_time,allocation_date,purpose from Resource_Allocation where  room_type=" + int.Parse(DropDownList1.SelectedValue) + " and allocation_date='" + txtdate.Text + "' and NOT((allocation_s_time>='" + txttotime.Text + "') or (allocation_e_time<='" + txtfromtime.Text + "')) ", cn);
+            DataSet ds_alloc = new DataSet();
+            da_alloc.Fill(ds_alloc);
+
+            SqlDataAdapter da_rooms = new SqlDataAdapter("select Rooms.*,Resource_Details.*,Authorised_user.Name,Authorised_user.Department,Authorised_user.Extension from Rooms inner join Resource_Details on Rooms.building=Resource_Details.Id inner join Authorised_user on Rooms.authorised_user= Authorised_user.Id where Rooms.Type=" + int.Parse(DropDownList1.SelectedValue) + " ", cn);
+            //select * from Rooms inner join Resource_Details on Rooms.building=Resource_Details.Id where Rooms.Type=;
+            DataSet ds_rooms = new DataSet();
+            da_rooms.Fill(ds_rooms);
+            //where Type=" + int.Parse(DropDownList1.SelectedValue) +"
+            ds_rooms.Tables[0].Columns.Add("Availability");
+            ds_rooms.Tables[0].Columns.Add("From");
+            ds_rooms.Tables[0].Columns.Add("To");
+            ds_rooms.Tables[0].Columns.Add("Date");
+            ds_rooms.Tables[0].Columns.Add("Purpose");
+            if (ds_alloc.Tables[0].Rows.Count == 0)
+            {
+
+                for (int i = 0; i < ds_rooms.Tables[0].Rows.Count; i++)
+                {
+                    ds_rooms.Tables[0].Rows[i]["Availability"] = "Available";
+                }
+
+
+
+            }
+            else
+            {
+                for (int i = 0; i < ds_rooms.Tables[0].Rows.Count; i++)
+                {
+
+
+                    ds_rooms.Tables[0].Rows[i]["Availability"] = "Available";
+                    ds_rooms.Tables[0].Rows[i]["From"] = "None";
+                    ds_rooms.Tables[0].Rows[i]["To"] = "None";
+                    ds_rooms.Tables[0].Rows[i]["Date"] = "None";
+                    ds_rooms.Tables[0].Rows[i]["Purpose"] = "None";
+                    for (int j = 0; j < ds_alloc.Tables[0].Rows.Count; j++)
+                    {
+                        if (int.Parse(ds_alloc.Tables[0].Rows[j][0].ToString()) == int.Parse(ds_rooms.Tables[0].Rows[i]["Id"].ToString()))
+                        {
+
+                            ds_rooms.Tables[0].Rows[i]["Availability"] = "Not Available";
+                            ds_rooms.Tables[0].Rows[i]["From"] = ds_alloc.Tables[0].Rows[j][1].ToString();
+                            ds_rooms.Tables[0].Rows[i]["To"] = ds_alloc.Tables[0].Rows[j][2].ToString();
+                            ds_rooms.Tables[0].Rows[i]["Date"] = ((DateTime)ds_alloc.Tables[0].Rows[j][3]).ToShortDateString();
+                            ds_rooms.Tables[0].Rows[i]["Purpose"] = ds_alloc.Tables[0].Rows[j][4].ToString();
+                            break;
+                            //ds_rooms.Tables[0].Rows[i]["Availability"] = "Not Available";
+                        }
+                    }
+
+
+
+
+
+                }
+            }
+            GridView1.DataSource = ds_rooms;
+            GridView1.DataBind();
+        }
+        else
+        {
+            GridView1.DataSource = null;
+            GridView1.DataBind();
+
+            SqlDataAdapter da_alloc = new SqlDataAdapter("select resource_id,allocation_s_time,allocation_e_time,allocation_date,purpose from Resource_Allocation where resource_id=" + int.Parse(DropDownList2.SelectedValue.ToString()) + " and room_type=" + int.Parse(DropDownList1.SelectedValue) + " and allocation_date='" + txtdate.Text + "' and NOT((allocation_s_time>='" + txttotime.Text + "') or (allocation_e_time<='" + txtfromtime.Text + "')) ", cn);
+            DataSet ds_alloc = new DataSet();
+            da_alloc.Fill(ds_alloc);
+
+            SqlDataAdapter da_rooms = new SqlDataAdapter("select Rooms.*,Resource_Details.*,Authorised_user.Name,Authorised_user.Department,Authorised_user.Extension from Rooms inner join Resource_Details on Rooms.building=Resource_Details.Id inner join Authorised_user on Rooms.authorised_user= Authorised_user.Id where Rooms.Type=" + int.Parse(DropDownList1.SelectedValue) + " and Rooms.Id=" + int.Parse(DropDownList2.SelectedValue.ToString()) + " ", cn);
+            //select * from Rooms inner join Resource_Details on Rooms.building=Resource_Details.Id where Rooms.Type=;
+            DataSet ds_rooms = new DataSet();
+            da_rooms.Fill(ds_rooms);
+            //where Type=" + int.Parse(DropDownList1.SelectedValue) +"
+            ds_rooms.Tables[0].Columns.Add("Availability");
+            ds_rooms.Tables[0].Columns.Add("From");
+            ds_rooms.Tables[0].Columns.Add("To");
+            ds_rooms.Tables[0].Columns.Add("Date");
+            ds_rooms.Tables[0].Columns.Add("Purpose");
+            if (ds_alloc.Tables[0].Rows.Count == 0)
+            {
+
+                for (int i = 0; i < ds_rooms.Tables[0].Rows.Count; i++)
+                {
+                    ds_rooms.Tables[0].Rows[i]["Availability"] = "Available";
+                }
+
+
+
+            }
+            else
+            {
+                for (int i = 0; i < ds_rooms.Tables[0].Rows.Count; i++)
+                {
+
+
+                    ds_rooms.Tables[0].Rows[i]["Availability"] = "Available";
+                    ds_rooms.Tables[0].Rows[i]["From"] = "None";
+                    ds_rooms.Tables[0].Rows[i]["To"] = "None";
+                    ds_rooms.Tables[0].Rows[i]["Date"] = "None";
+                    ds_rooms.Tables[0].Rows[i]["Purpose"] = "None";
+                    for (int j = 0; j < ds_alloc.Tables[0].Rows.Count; j++)
+                    {
+                        if (int.Parse(ds_alloc.Tables[0].Rows[j][0].ToString()) == int.Parse(ds_rooms.Tables[0].Rows[i]["Id"].ToString()))
+                        {
+
+                            ds_rooms.Tables[0].Rows[i]["Availability"] = "Not Available";
+                            ds_rooms.Tables[0].Rows[i]["From"] = ds_alloc.Tables[0].Rows[j][1].ToString();
+                            ds_rooms.Tables[0].Rows[i]["To"] = ds_alloc.Tables[0].Rows[j][2].ToString();
+                            ds_rooms.Tables[0].Rows[i]["Date"] = ((DateTime)ds_alloc.Tables[0].Rows[j][3]).ToShortDateString();
+                            ds_rooms.Tables[0].Rows[i]["Purpose"] = ds_alloc.Tables[0].Rows[j][4].ToString();
+                            break;
+                            //ds_rooms.Tables[0].Rows[i]["Availability"] = "Not Available";
+                        }
+                    }
+
+
+
+
+
+                }
+            }
+            GridView1.DataSource = ds_rooms;
+            GridView1.DataBind();
+        }
+       
+    }
+    protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+       Response.Write(e.CommandArgument.ToString());
+      // Response.Write(GridView1.SelectedRow.RowIndex.ToString());
+       SqlCommand cmd = new SqlCommand("insert into Resource_Allocation values ('" + txtdate.Text + "','" + txtfromtime.Text + "','" + txttotime.Text + "'," + int.Parse(e.CommandArgument.ToString()) + ",'" + txtpurpose.Text + "',0," + int.Parse(DropDownList1.SelectedValue) + ") ", cn);
+       cn.Open();
+       cmd.ExecuteNonQuery();
+       cn.Close();
+
+    }
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+    protected void btnbook_Command(object sender, CommandEventArgs e)
+    {
+        //Response.Write(e.CommandArgument.ToString());
+        
+       //SqlCommand cmd=new SqlCommand("insert into Resource_Allocation ")
+    }
+    
+    protected void GridView1_DataBound(object sender, EventArgs e)
+    {
+        for (int i = 0; i < GridView1.Rows.Count; i++)
+        {
+            if (((Label)GridView1.Rows[i].Cells[1].FindControl("lblavailability")).Text == "Available")
+            {
+                //((Button)GridView1.Rows[i].Cells[0].FindControl("btnbook")).Enabled = true;
+                GridView1.Rows[i].Enabled = true;
+            }
+            else
+            {
+                //((Button)GridView1.Rows[i].Cells[0].FindControl("btnbook")).Enabled = false;
+                GridView1.Rows[i].Enabled = false;
+            }
+
+        }
+    }
+}
